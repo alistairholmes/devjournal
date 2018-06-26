843a2dbd0cf376bb99f7f83ee57edcdc9016391e
alistairholmes <alistairholmes95@gmail.com>
2018-06-26 15:37:30 +0200

    Init commit

---
+++ app/src/main/java/com/alistairholmes/devjournal/database/JournalEntry.java
@@ -0,0 +1,60 @@
+package com.alistairholmes.devjournal.database;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "Journal Entries")
+public class JournalEntry {
+
+    //Variables names for the columns in the database table
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String description;
+    @ColumnInfo(name = "updated_at")
+    private Date updated_at;
+
+    @Ignore
+    public JournalEntry(String title, String description, Date updated_at) {
+        this.title = title;
+        this.description = description;
+        this.updated_at = updated_at;
+    }
+
+    public JournalEntry(int id, String title, String description, Date updated_at) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.updated_at = updated_at;
+    }
+
+    //Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+}
