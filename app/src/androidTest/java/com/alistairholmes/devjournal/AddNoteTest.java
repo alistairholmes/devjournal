778498531c0da0dd62067da3450a7b5cb4d82315
Shou Xian <renegod9@gmail.com>
2018-10-11 08:54:08 +0800

    Initial steps of the tests for adding a note

---
+++ app/src/androidTest/java/com/alistairholmes/devjournal/AddNoteTest.java
@@ -0,0 +1,51 @@
+package com.alistairholmes.devjournal;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.alistairholmes.devjournal.customViewAsserts.RecyclerViewMatcher;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class AddNoteTest {
+
+    @Rule
+    public ActivityTestRule<JournalActivity> mActivityRule =
+            new ActivityTestRule<>(JournalActivity.class);
+
+    @Test
+    public void addNewNote() {
+        String noteTitle = "Test Note " + System.currentTimeMillis();
+        String noteDescription = "Test Description " + System.currentTimeMillis();
+
+        // Opens AddEntryActivity
+        onView(withId(R.id.fab)).perform(click());
+
+        // Key in information in AddEntryActivity
+        onView(withId(R.id.editTextTitle)).perform(typeText(noteTitle));
+        onView(withId(R.id.editTextEntryDescription)).perform(typeText(noteDescription)).perform(closeSoftKeyboard());
+        onView(withId(R.id.fab_update)).perform(click());
+
+       onView(withRecyclerView(R.id.recyclerView_journalEntries).atPositionOnView(0, R.id.entry_title))
+               .check(matches(hasDescendant(withText(noteTitle))));
+    }
+
+    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+        return new RecyclerViewMatcher(recyclerViewId);
+    }
+}
