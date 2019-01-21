8eacff81d4156b079cb44124750e10e8a530d2f9
alistair <alistairholmes95@gmail.com>
2019-01-21 14:39:05 +0200

    Created new project structure.

---
+++ app/src/main/java/com/alistairholmes/devjournal/viewmodels/JournalViewModel.java
@@ -0,0 +1,14 @@
+package com.alistairholmes.devjournal.viewmodels;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class JournalViewModel extends AndroidViewModel {
+
+    public JournalViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+
+}
